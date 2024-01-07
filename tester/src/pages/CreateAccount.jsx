b7f02395d030a7a0a7a5fc3f79ce94952f1ebf7e
hftrujillo <hftrujillo@loyola.edu>
2024-01-06 22:16:05 -0500

    Finished Auth UI

--- tester/src/pages/CreateAccount.jsx
+++ tester/src/pages/CreateAccount.jsx
@@ -1,5 +1,4 @@
 import { Header, Footer } from '../sections';
-
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { supabase } from '../Supabase';
@@ -8,7 +7,18 @@ const CreateAccount = () => {
   return (
     <createAccount>
         <Header />
-        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }}/>
+        <Auth supabaseClient={supabase} providers={['google']} appearance={{
+
+            extend: false,
+            className: {
+                button: "btn btn-primary bg-dashing-pink hover:bg-[#db5656] text-white border-dashing-pink hover:border-dashing-pink hover:text-[#c0c0c0]",
+                container: "flex flex-col items-center gap-4 bg-cweam pb-2",
+                input: "bg-white rounded-md p-2 flex-row",
+                divider: "bg-cweam p-2",
+                label: "text-navy-sucks pr-4",
+                anchor: "text-navy-sucks hover:text-[#5e7aeb]"
+            }
+        }}/>
         <Footer />
     </createAccount>
   )
